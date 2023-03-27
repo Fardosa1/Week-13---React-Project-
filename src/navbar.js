01457f57bdf87fc0b97afb843395199af4a28e64
Fardowsaa <fardowsaa1@Fardowsaas-MacBook-Air.local>
2023-03-26 21:01:44 -0500

    finalpush

--- src/navbar.js
+++ src/navbar.js
@@ -12,3 +12,11 @@ const Navbar = () => {
 };
 
 export default Navbar;
+ 
+// This portion of the code that uses react component code that creates a component
+// called Navbar! :) This component is a function component that rerutrn a navigation element with the
+// class name navbar. Inside the nav element there is three
+// a elements that have links with the text Link 1 2 & 3 
+// the links don't go anywhere since we weren't required to!
+// then last the export default navbar is used to be imported and used
+// in another parts of the code!
